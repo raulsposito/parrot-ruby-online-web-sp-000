23c0830c29c4b420b511ad7a11b97c48be6d8efb
Raul Sposito <raulsposito@gmail.com>
2019-06-25 13:49:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase, "Squawk!", "Pretty bird!")
-  puts phrase 
+def parrot("Squawk!", "Pretty bird!")
+  puts [1] 
   return phrase 
 end
